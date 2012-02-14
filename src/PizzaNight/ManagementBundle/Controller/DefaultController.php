f042b2f167b5d8e4068743ca6bb8141a6b76e99e
Ariel Ferrandini <arielferrandini@gmail.com>
2012-02-14 02:26:07 +0100

    Inicio de seguridad

--- src/PizzaNight/ManagementBundle/Controller/DefaultController.php
+++ src/PizzaNight/ManagementBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace PizzaNight\ManagementBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
@@ -30,4 +31,28 @@ class DefaultController extends Controller
 
         return array('attendees' => $attendees);
     }
+
+    /**
+     * @Route("/login", name="_login")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+            // last username entered by the user
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        );
+    }
+
 }
